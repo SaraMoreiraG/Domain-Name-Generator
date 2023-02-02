30171e5acf4dc3e8c624ed04dcf1e87aacad1ba2
SaraMoreiraG <sara_moreira@hotmail.es>
2023-02-02 19:04:45 +0000

    Done!

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function domainNameGenerator() {
+    let pronoun = ["the", "our", "mine", "your"];
+    let adj = ["great", "big", "small", "pretty"];
+    let noun = ["jogger", "racoon", "elephant", "door"];
+
+    let pronounNumber = Math.floor(Math.random() * 4);
+    let adjNumber = Math.floor(Math.random() * 4);
+    let nounNumber = Math.floor(Math.random() * 4);
+
+    let domainName =
+      pronoun[pronounNumber] + adj[adjNumber] + noun[nounNumber] + ".com";
+    return domainName;
+  }
+  console.log(domainNameGenerator());
 };
